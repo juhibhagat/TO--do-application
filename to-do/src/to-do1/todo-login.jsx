3440f475d207240499860c76113d5fa6b2270e25
juhibhagat <jkri0206@gmail.com>
2026-02-08 17:28:10 +0530

    create login page and navigation

--- to-do/src/to-do1/todo-login.jsx
+++ to-do/src/to-do1/todo-login.jsx
@@ -1,19 +1,90 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useFormik } from "formik";
 
+export function ToDologin() {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
 
+  // Load users from json-server
+  function loadUsers() {
+    axios
+      .get("http://localhost:3000/users")
+      .then((response) => setUsers(response.data))
+      .catch(() => alert("Server not running"));
+  }
 
-export function ToDologin(){
-  
-    return(
-        <div className="container-fluid">
-            <form className="w-75 border border-5">
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
-                <div>User_id</div>
-                <div><input type="text" /></div>
-                <div>Password</div>
-                <div><input type="text" /></div>
-                <button className="btn btn-primary m-4">Login</button>
-            </form>
-            
+  const formik = useFormik({
+    initialValues: {
+      user_id: "",
+      password: "",
+    },
+
+    onSubmit: (formData) => {
+      const userDetails = users.find(
+        (item) => item.user_id.toString() === formData.user_id
+      );
+
+      if (!userDetails) {
+        alert("User not found");
+        return;
+      }
+
+      if (userDetails.password === formData.password) {
+        // save login user (optional but useful)
+        localStorage.setItem("login_user", formData.user_id);
+
+        navigate("/dashboard");
+      } else {
+        alert("Invalid password");
+      }
+    },
+  });
+
+  return (
+    <div className="container-fluid">
+      <form className="p-4" onSubmit={formik.handleSubmit}>
+        <h4 className="bi bi-person-fill"></h4>
+
+        <dl>
+          <dt>User Id</dt>
+          <dd>
+            <input
+              type="text"
+              name="user_id"
+              className="form-control"
+              value={formik.values.user_id}
+              onChange={formik.handleChange}
+              required
+            />
+          </dd>
+
+          <dt>Password</dt>
+          <dd>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              required
+            />
+          </dd>
+        </dl>
+
+        <button type="submit" className="btn btn-primary w-100">
+          Login
+        </button>
+
+        <div className="mt-4">
+          <Link to="/register">New User register</Link>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
--- to-do/src/to-do1/todo-register.jsx
+++ to-do/src/to-do1/todo-register.jsx
@@ -1,40 +1,44 @@
 import { useFormik } from "formik"
 import axios from 'axios'
+import { Link } from "react-router-dom"
 
 
-export function ToDoRegister(){
+export function ToDoRegister() {
 
-const formik = useFormik({
-    initialValues : {
-        user_id : "",
-        user_name :"",
-        password : "",
-        email : "",
-    },
-    onSubmit: (user) =>{
-        axios.post(`http://localhost:3000/users`, user)
-        .then(()=>{
-            console.log('registered')
-        })
-        alert('Register Succesfully...')
-    }
-})
-    return(
+    const formik = useFormik({
+        initialValues: {
+            user_id: "",
+            user_name: "",
+            password: "",
+            email: "",
+        },
+        onSubmit: (user) => {
+            axios.post(`http://localhost:3000/users`, user)
+                .then(() => {
+                    console.log('registered')
+                })
+            alert('Register Succesfully...')
+        }
+    })
+    return (
         <div className="container-fluid">
             <form className="p-4 w-50" onSubmit={formik.handleSubmit}>
                 <dl>
                     <dt>User_Id</dt>
-                      <dd><input type="number" className="form-control" name="user_id"  onChange={formik.handleChange}/></dd>
+                    <dd><input type="number" className="form-control" name="user_id" onChange={formik.handleChange} /></dd>
                     <dt>User_name</dt>
-                    <dd><input type="text" className="form-control" name="user_name"  onChange={formik.handleChange} /></dd>
+                    <dd><input type="text" className="form-control" name="user_name" onChange={formik.handleChange} /></dd>
                     <dt>Password</dt>
-                    <dd><input type="password" className="from-control" name="password"  onChange={formik.handleChange} /></dd>
+                    <dd><input type="password" className="from-control" name="password" onChange={formik.handleChange} /></dd>
                     <dt>Email</dt>
-                    <dd><input type="email" className="from-control" name="email"  onChange={formik.handleChange} /></dd>
+                    <dd><input type="email" className="from-control" name="email" onChange={formik.handleChange} /></dd>
                 </dl>
                 <button className="btn btn-primary w-25" type="submit" > Register</button>
+                <div className="mt-4">
+                    <Link to="/login">go to login page</Link>
+                </div>
             </form>
-            
+
         </div>
     )
 }
